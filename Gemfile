f8b4b71f06a8726ceb8d9e166581a03a2065f1c8
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-05-26 13:00:07 +0900

    テストコードの実装

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'mysql2', '>= 0.4.4'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
-
+gem 'sass-rails', '~> 5'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 4.0'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -31,7 +31,12 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
-  gem 'rspec-rails', '~>4.0.0'
+  gem 'rspec-rails'
+  gem 'factory_bot_rails'
+  gem 'faker'  
+  gem 'gimei'
+
+  
 end
 
 group :development do
@@ -56,3 +61,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'pry-rails'
+
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :nickname,    presence: true
+  validates :family_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ン一-龥]/}
+  # KATAKANA_REGEXP = /\A[\p{katakana}\u{30fc}]+\z/
+  validates :family_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
+  validates :first_name_kana, presence: true,format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/}
   validates :birth_day,     presence: true   
 end
