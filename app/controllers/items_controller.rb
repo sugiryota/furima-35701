ddcad3768ab8ec850da94f07be9026de2f0a56b8
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-05-27 18:51:56 +0900

    issue4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,7 +46,7 @@ class ItemsController < ApplicationController
   end
   def move_to_index
     @item=Item.find(params[:id])
-    unless user_signed_in? && @item.user_id == current_user.id
+    unless user_signed_in? && @item.user_id
       redirect_to action: :index
     end
   end
