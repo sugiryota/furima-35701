7377647a9737458a5aeafe9f5f77469f12aebb50
sugiryota <83449662+sugiryota@users.noreply.github.com>
2021-05-27 14:44:54 +0900

    issue4

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -41,6 +41,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+      it 'priceが半角英数混合ではできないこと' do
+        @item.price ="44a" 
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it 'priceが半角英語だけでは登録できないこと' do
+        @item.price = "abc"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
       it '画像を1枚つけることが必須であること' do
         @item.image = nil
         @item.valid?
